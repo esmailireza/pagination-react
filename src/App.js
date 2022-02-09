e718c261d38f290d0e6f15ce6c7b7d440bb9b64d
esmailireza <esmailireza20@gmail.com>
2022-02-09 13:45:12 +0330

    create states and fetch api using axios library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "./App.css";
-
+import Posts from "./components/Posts";
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [PostsperPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
-      <p>react</p>
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={posts} loading={loading} />
     </div>
   );
 }
